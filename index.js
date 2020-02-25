bdf5bfe252ebc91e672f9e6a4c93d742f9beae92
Rui Correia <rmgc31@gmail.com>
2020-02-25 22:12:12 +0000

    search by title implemented

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const app = require('./src/app');
 const APP_PORT = 3000;
 
 app.listen(APP_PORT, () => {
-  console.log(`Now serving your Express app at http://localhost:${APP_PORT}`); // eslint-disable-line
+  console.log('App listening on port 3000.'); // eslint-disable-line
 });
--- src/app.js
+++ src/app.js
@@ -1,10 +1,39 @@
+// Loading express framework.
 const express = require('express');
 
+// Creating instance of express to serve endpoints.
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({
-    message: 'Hello world!',
+// Loading the body-parser helper.
+app.use(express.json());
+
+// Loading request packages.
+const axios = require('axios');
+const JustWatch = require('justwatch-api');
+
+// Routes.
+app.get('/titles/:title', async (request, response) => {
+  const { title } = request.params;
+  const { OMBD_API, OMBD_API_KEY } = process.env;
+  const jw = new JustWatch({ locale: 'en_GB' });
+
+  const imbdResponse = await axios.get(OMBD_API, {
+    params: {
+      t: title,
+      apikey: OMBD_API_KEY,
+      plot: 'full',
+    },
+  });
+
+  const justWatchResponse = await jw.search({
+    query: title,
+    page: 1,
+    page_size: 1,
+  });
+
+  response.status(200).json({
+    imbd: imbdResponse.data,
+    justWatch: justWatchResponse.items.pop(),
   });
 });
 
